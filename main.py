dc1051e7d331b991e7e0fad4c0c9991cbe7042d1
Siddharth Raghavan <mowglu@gmail.com>
2022-01-15 16:54:32 -0500

    Just adding a sample main.py, not sure of use case. Will build as I use this.

---
+++ main.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+import pandas as pd
+
+
+def my_func():
+    flightlist = pd.concat(
+        pd.read_csv(file, parse_dates=["firstseen", "lastseen", "day"])
+        for file in Path("path/to/folder").glob("flightlist_*.csv.gz")
+    )
+
+
+def main_wrapper():
+    print(
+        f"This is the start of our python project, we will be starting off with this wrapper main function called {main_wrapper.__name__}")
+
+
+if __name__ == "__main__":
+    main_wrapper()
+    # Just a comment.
