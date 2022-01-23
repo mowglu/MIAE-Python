5148ed9460e50ffed0c2ebed7aff9868f949b05c
Siddharth Raghavan <mowglu@gmail.com>
2022-01-22 22:32:12 -0500

    Added examples of advanced python typecasting, docstringing, return typing, TODOing

---
+++ api/utils.py
@@ -0,0 +1,17 @@
+def solid_example_1(*, example_param_1: str, example_param_2: int) :
+    """
+
+    :param example_param_1:
+    :param example_param_2:
+    :return:
+    """
+    #TODO: Some todo's are super useful
+
+
+def solid_example_2(example_param_1: float) -> int:
+    """
+
+    :param example_param_1:
+    :return:
+    """
+    return 2
