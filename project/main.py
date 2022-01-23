8b971b69d82ac32520edfd7ba7c54fe970525c35
Siddharth Raghavan <mowglu@gmail.com>
2022-01-23 11:07:39 -0500

    Modularized more

--- project/main.py
+++ project/main.py
@@ -1,23 +1,15 @@
-from pathlib import Path
-import pandas as pd
 from api import fetcher
-from data import a_v_data
-
-def fix_dataset() -> pd.DataFrame:
-    flightlist = pd.concat(
-        pd.read_csv(file, parse_dates=["firstseen", "lastseen", "day"])
-        # for file in Path("path/to/folder").glob("flightlist_*.csv.gz")
-        for file in Path("C:/Users/mowgl/Documents/Python/MIAE_Python_Tutorial/MIAE-Python/data_set").glob("flightlist_*.csv.gz")
-    )
-    return flightlist
+from data import a_v_data, aggregate
 
 
 def main_wrapper():
     print(
         f"This is the start of our python project, we will be starting off with this wrapper main function called {main_wrapper.__name__}")
     fetcher.states_accessor()
-    flight_list = fix_dataset()
+    flight_list = aggregate.fix_dataset()
     a_v_data.analyze(dataset=flight_list)
+    print("This is the end of our python project")
+
 
 if __name__ == "__main__":
     main_wrapper()
