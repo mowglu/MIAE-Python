d93a9401b5e7d650c7a5a6562bea8c8c5e8a22cf
Siddharth Raghavan <mowglu@gmail.com>
2022-01-29 03:24:41 -0500

    #10. Commit

--- project/main.py
+++ project/main.py
@@ -57,9 +57,17 @@ def main_wrapper():
     #9. Visualizer is a_v_data script.
     a_v_data.visualize(data=cleaned_data, airports_subset=AIRPORTS_BY_REGION)
 
-    #10.
     print("This is the end of our python project")
 
 
 if __name__ == "__main__":
     main_wrapper()
+
+    #10. Future ideas.
+    # SPIN off from this project, you can get creative. Some ideas:
+    # 1. Implement usage of timezones within your unix time converters
+    # 2. Use matplotlib or seaborn instead of altair. Can you get equally visually pleasing graphs?
+    # 3. Use only the "formed" data. Clean it up the best you can and visualize the data. Can you use other columns to
+    # your advantage? For example, maybe you could determine that an departing flight is only considered so if the
+    # "departure_airport_candidates_count" is lower than a certain threshold (think env variables).
+    # 4. Use another API you find on the web and go ham. Explore!
