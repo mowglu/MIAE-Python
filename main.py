09c89b32d9baedb19126fcea4810416d3c81d97c
Siddharth Raghavan <mowglu@gmail.com>
2022-01-29 00:35:53 -0500

    1. commit message.

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
 # Let's start in main.py showing some wrapper patterns and depicting method attributes.
 # Explore with pushes and pulls and view on GitHub!
-def main_wrapper():
-    # intrinsic methods
-    print(
-        f"This is the start of our python project, we will be starting off with this wrapper main function called {main_wrapper.__name__}")
-
-    # Stuff here - wrapper!
-
-    print("This is the end of our python project")
-
-
-if __name__ == "__main__":
-    main_wrapper()
+# git remote add new_origin https://github.com/mowglu/MIAE-Python
+# git pull new_origin 2221c00bbb739e2033fc5016476ad7caa95b7bc9
